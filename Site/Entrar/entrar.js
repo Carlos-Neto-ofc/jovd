3dc78970bf54a4809f21d854882ceeffa80f40de
Carlos Neto <carlos.neto.costa999@gmail.com>
2026-01-13 16:27:28 -0300

    Add files via upload

--- Site/Entrar/entrar.js
+++ Site/Entrar/entrar.js
@@ -1,15 +1,15 @@
-const login = document.
-getElementById('login');
-const entrar = document.
-getElementById('entrar');
-const register = document.
-getElementById("registrar");
 
-register.addEventListener('click', () => {
-login.classList.add('active');
 
-});
-entrar.addEventListener('click', () => {
-login.classList.remove('active');
+    const login = document.getElementById('login');
+    const entrar = document.getElementById('entrar');
+    const register = document.getElementById("registrar");
+    const googleico = document.getElementById('googleico');
 
-});
+    register.addEventListener('click', () => {
+        login.classList.add('active');
+    });
+
+    entrar.addEventListener('click', () => {
+        login.classList.remove('active');
+    });
+
